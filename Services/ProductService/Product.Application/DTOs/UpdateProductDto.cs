7f9f55646b48b220a2020b3a1b96563b705e22da
Aniket Hiware <anikethiware3@gmail.com>
2026-01-19 22:36:29 +0530

    complete products crud

--- Services/ProductService/Product.Application/DTOs/UpdateProductDto.cs
+++ Services/ProductService/Product.Application/DTOs/UpdateProductDto.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Product.Application.DTOs
-{
-    public class CreateProductDto
+{ 
+    public class UpdateProductDto
     {
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
@@ -16,6 +16,7 @@ namespace Product.Application.DTOs
         public int CategoryId { get; set; }
         public string SKU { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
     }
 
 }
