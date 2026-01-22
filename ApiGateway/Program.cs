00b1bb76cead2236435d64c6062bbc58e83cfaa3
Aniket Hiware <anikethiware3@gmail.com>
2026-01-22 23:37:37 +0530

    add domain reference and cors enabled for 4200

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -4,6 +4,16 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("AllowAngular");
 await app.UseOcelot();
 app.Run();
 
